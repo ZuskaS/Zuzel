b9accb18d58868c6e5efd19d7f9649dfa8a4d6dc
Eliška Štěpánová <stepanova.eliska01@gmail.com>
2025-06-04 18:19:37 +0200

    Přidání komponenty App a router v index.jsx. Přidání složky img s ikonami.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,28 @@
 import { createRoot } from 'react-dom/client';
-import { HomePage } from './pages/HomePage/homepage';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { App } from './Components/App/App';
+import { HomePage } from './pages/HomePage/HomePage';
 import './global.css';
 
-createRoot(document.querySelector('#app')).render(<HomePage />);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+    //      {
+    //     path: '/...',
+    //     element: <...HomePage. />,
+    //   },
+    ]
+  }
+]);
+
+createRoot(document.querySelector('#app')).render(
+  <RouterProvider router={router} />
+);
