30301fbf86e8236fad5bc87b8e0433c41fdd6aad
Eliška Štěpánová <stepanova.eliska01@gmail.com>
2025-06-06 13:56:15 +0200

    Přidání části obsahu do TipsPage

---
+++ src/pages/TipsPage/TipsPage.jsx
@@ -0,0 +1,27 @@
+import './img/gear.png'
+import './TipsPage.css'
+import sound_on from '../../Components/Header/img/sound_on.png';
+import ZUZEL from '../HomePage/img/Zuzel.png';
+
+
+export const TipsPage = () => {
+    return (
+        <div>
+            <div className="page-title">
+                <p>Tipy k ovládání aplikace</p>
+                <img className="icon_gear" src={gear} alt="ikona ozubeného kola" />
+            </div>
+
+            <div>
+                <p> Zvuky aplikace lze zapnout nebo vypnout ikonou       v pravém horním rohu. </p>
+                <img className='icon_sound' src={sound_on} alt='ikona zvuku' />
+            </div>
+
+            <div><p>Ikona loga v levém horním rohu Vás vrátí na úvodní stranu.</p>
+                <img className="img__logo" src={ZUZEL} alt="logo ZUZEL" />
+            </div>
+        </div>
+
+
+    )
+}
