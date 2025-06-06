cf006e60dca0e1ca0f57f4ba6f9e0165a21958c7
Zuzana Ilečková <yes>
2025-06-06 21:28:36 +0200

    nahrání ilustračních obrázků s mřížkou, přesunutí obrázků k odpovídajícím pages, přejmenování některých z nich pro zjednodušení, vytvoření jsx InstroctionsPage a jejího css včetně druhé strany, kde je nastavena třída pro možnost zobrazování jedné nebo druhé třídy, pokud to bude možné.

---
+++ src/pages/InstructionsPage/InstructionsPage.jsx
@@ -0,0 +1,59 @@
+import './img/info.png';
+import './InstructionsPage.css';
+import '.img/instruction__grid.png';
+import '.img/lupa.png';
+import '.img/hand.png';
+import '.img/instruction__gridEmpty';
+
+export const InstructionsPages = () => {
+  return (
+    <div>
+      <div className="first__page">
+        <div className="page-title">
+          <p>Návod</p>
+          <img className="icon__info" src={info} alt="ikona informací" />
+        </div>
+
+        <div className="info__text">
+          <p> Cílem cvičení je posílit krátkodobou pamět. </p>
+        </div>
+
+        <div className="instuction__visual">
+          <img className="instruction__lupa" src={lupa} alt="lupa" />
+          <img
+            className="instruction__grid"
+            src={instruction__grid}
+            alt="obrázek mřížky se 3 vyplněnými poli"
+          />
+        </div>
+
+        <div className="info__text">
+          <p>V prvním kroku si zapamatujete barevný vzor v mřížce.</p>
+        </div>
+      </div>
+
+      <div className="second__page">
+        <div>
+          <img
+            className="second__page--infoImg"
+            src={info}
+            alt="ikona informací"
+          />
+        </div>
+
+        <div className="instuction__visual--empty">
+          <img className="hand" src={hand} alt="ruka ukazující ukazovákem" />
+          <img
+            className="instruction__grid--empty"
+            src={instruction__gridEmpty}
+            alt="obrázek mřížky se prázdnými poli"
+          />
+        </div>
+
+        <div className="info__text">
+          <p> Barvu měníte opakovaným kliknutím na čtverec. </p>
+        </div>
+      </div>
+    </div>
+  );
+};
