86ee71d71cf55e573402ff86bdc1960b3f27fc77
Eliška Štěpánová <stepanova.eliska01@gmail.com>
2025-06-06 10:54:23 +0200

    Přidání do Header obrázek na přepínání zvuku a odkaz na HomePage po kliku na logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,31 @@
 import './Header.css';
 import ZUZEL from '../../pages/HomePage/img/Zuzel.png';
 import sound_on from './img/sound_on.png';
+import mute from './img/mute.png';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
+    const [sound, setSound] = useState(true)
+
+    const toggleSound = () => {
+        setSound(!sound)
+    }
+
     return (
         <div className="header">
-            <img className="logo" src={ZUZEL} alt="logo ZUZEL" />
-            <img className="sound_on" src={sound_on} alt="zvuk zapnut" />
+            <Link to='/'>
+                <img
+                    className="logo"
+                    src={ZUZEL}
+                    alt="logo ZUZEL" />
+            </Link>
+            <img
+                onClick={toggleSound}
+                className="sound"
+                src={sound ? sound_on : mute}
+                alt={sound ? "zvuk zapnuto" : "zvuk vypnuto"}
+            />
         </div>
     )
 }
