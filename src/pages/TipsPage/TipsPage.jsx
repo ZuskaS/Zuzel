b0858c2b397fe354f632c704abfdba103751c9d5
Zuzana Ilečková <yes>
2025-06-07 14:11:27 +0200

    úprava stylů tips pages a s tím souvisejících.

--- src/pages/TipsPage/TipsPage.jsx
+++ src/pages/TipsPage/TipsPage.jsx
@@ -1,50 +1,69 @@
-import gear from './img/gear.png'
-import './TipsPage.css'
+import gear from './img/gear.png';
+import mute from '../../Components/Header/img/mute.png';
+import './TipsPage.css';
 import sound_on from '../../Components/Header/img/sound_on.png';
 import ZUZEL from '../HomePage/img/Zuzel.png';
-import { PageTitle } from '../../Components/PageTitle/PageTitle'
+import { PageTitle } from '../../Components/PageTitle/PageTitle';
 import { Button } from '../../Components/Button/Button';
-import arrowRight from '../../img/arrow-right.png'
+import arrowRight from '../../img/arrow-right.png';
 import { useState } from 'react';
 
 export const TipsPage = () => {
-    const [page, setPage] = useState(true)
+  const [page, setPage] = useState(true);
 
-    return (
-        <>
-            {page ? (
-                <div className='tips-1'>
-                    <PageTitle>
-                        <img className="icon_gear" src={gear} alt="ikona ozubeného kola" />
-                        <p>Tipy k ovládání aplikace</p>
-                    </PageTitle>
+  return (
+    <>
+      {page ? (
+        <div className="tips-1">
+          <PageTitle>
+            <img className="icon_gear" src={gear} alt="ikona ozubeného kola" />
+            <p>Tipy k ovládání aplikace</p>
+          </PageTitle>
 
-                    <div className="info__text">
-                        <p> Zvuky aplikace lze zapnout nebo vypnout ikonou       v pravém horním rohu. </p>
-                        <img className='icon_sound' src={sound_on} alt='ikona zvuku' />
-                    </div>
+          <div className="info__text">
+            <p>
+              {' '}
+              Zvuky aplikace lze zapnout nebo vypnout ikonou v pravém horním
+              rohu.{' '}
+            </p>
+            <div className="sound_icons">
+              <img className="icon_sound" src={sound_on} alt="ikona zvuku" />
+              <img className="mute" src={mute} alt="ikona vypnutého zvuku" />
+            </div>
+          </div>
 
-                    <div className="info__text">
-                        <p>Ikona loga v levém horním rohu Vás vrátí na úvodní stranu.</p>
-                        <img className="img__logo" src={ZUZEL} alt="logo ZUZEL" />
-                    </div>
+          <div className="info__text">
+            <p>Ikona loga v levém horním rohu Vás vrátí na úvodní stranu.</p>
+            <img className="img__logo" src={ZUZEL} alt="logo ZUZEL" />
+          </div>
 
-                    <Button onClick={() => setPage(false)} text='další' image={arrowRight} position='right' />
-                </div >
-            ) : (
+          <Button
+            onClick={() => setPage(false)}
+            text="další"
+            image={arrowRight}
+            position="right"
+          />
+        </div>
+      ) : (
+        <div className="tips-2">
+          <div className="info__text">
+            <p>
+              Pokud aplikace nereaguje, ujistěte se, že se obrazovky nedotýkáte
+              jinými prsty, např. při držení telefonu.
+            </p>
+          </div>
 
-                <div className='tips-2'>
-                    <div className="info__text">
-                        <p>Pokud aplikace nereaguje, ujistěte se, že se obrazovky nedotýkáte jinými prsty, např. při držení telefonu.</p>
-                    </div>
-
-                    <div className="info__text">
-                        <p>Pro pohodlnější ovládání doporučujeme použít dotykové pero.</p>
-                    </div>
-                    <Button onClick={() => setPage(true)} text='zpět' image={arrowLeft} position='left' />
-
-                </div>
-            )}
-        </>
-    )
-}
+          <div className="info__text">
+            <p>Pro pohodlnější ovládání doporučujeme použít dotykové pero.</p>
+          </div>
+          <Button
+            onClick={() => setPage(true)}
+            text="zpět"
+            image={arrowLeft}
+            position="left"
+          />
+        </div>
+      )}
+    </>
+  );
+};
