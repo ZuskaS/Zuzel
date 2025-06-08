f550e11b393612e509e99e5d20a767f20e936814
Zuzana Ilečková <yes>
2025-06-08 09:28:56 +0200

    úprava stylů

--- src/pages/DifficultySelectionsPage/DifficultySelectionsPage.jsx
+++ src/pages/DifficultySelectionsPage/DifficultySelectionsPage.jsx
@@ -1,6 +1,7 @@
 import { Button } from '../../Components/Button/Button';
 import { Link } from 'react-router-dom';
 import { PageTitle } from '../../Components/PageTitle/PageTitle';
+import './DifficultySelectionsPage.css';
 
 export const DifficultySelectionsPage = () => {
   return (
@@ -8,22 +9,23 @@ export const DifficultySelectionsPage = () => {
       <PageTitle>
         <p>Vyberte svoji úroveň</p>
       </PageTitle>
+      <div className="dificulty_container">
+        <Link to="/game/difficulty/1">
+          <Button text="Začátečník" position="center" />
+        </Link>
 
-      <Link to='/game/difficulty/1'>
-        <Button text="Začátečník" position="center" />
-      </Link>
+        <Link to="/game/difficulty/2">
+          <Button text="Mírně pokročilý" position="center" />
+        </Link>
 
-      <Link to='/game/difficulty/2'>
-        <Button text="Mírně pokročilý" position="center" />
-      </Link>
+        <Link to="/game/difficulty/3">
+          <Button text="Pokročilý" position="center" />
+        </Link>
 
-      <Link to='/game/difficulty/3'>
-        <Button text="Pokročilý" position="center" />
-      </Link>
-
-      <Link to='/game/difficulty/4'>
-        <Button text="Expert" position="center" />
-      </Link>
+        <Link to="/game/difficulty/4">
+          <Button text="Expert" position="center" />
+        </Link>
+      </div>
     </>
   );
 };
--- src/pages/HomePage/homepage.jsx
+++ src/pages/HomePage/homepage.jsx
@@ -2,34 +2,30 @@ import { Link } from 'react-router-dom';
 import './HomePage.css';
 import ZUZEL from './img/ZUZEL.png';
 import { Button } from '../../Components/Button/Button';
-import arrowRight from '../../img/arrow-right.png'
-import arrowLeft from '../../img/arrow-left.png'
+import arrowRight from '../../img/arrow-right.png';
+import arrowLeft from '../../img/arrow-left.png';
 
 export const HomePage = () => {
   return (
-    <div className="container">
-      <div className="homepage">
-        <img className="img__logo" src={ZUZEL} alt="logo ZUZEL" />
-        <h1>CVIČENÍ PAMĚTI</h1>
-        <div className="logo__grid">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-        <Link to='/tips'>
-          <Button text='Připravit se na trénink' />
-          {/* <Button text='Připravit se na trénink' image={arrowRight} position='right' />
-          <Button text='Připravit se na trénink' image={arrowLeft} position='left' /> */}
-
-
-        </Link>
+    <div className="homepage">
+      <img className="img__logo-home" src={ZUZEL} alt="logo ZUZEL" />
+      <h1>CVIČENÍ PAMĚTI</h1>
+      <div className="logo__grid">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
+      <Link to="/tips">
+        <Button text="Připravit se na trénink" />
+        {/* <Button text='Připravit se na trénink' image={arrowRight} position='right' />
+          <Button text='Připravit se na trénink' image={arrowLeft} position='left' /> */}
+      </Link>
     </div>
   );
 };
