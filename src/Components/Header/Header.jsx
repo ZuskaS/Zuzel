95c09fe399ee1168bd8225afc25a55b31fc84a3a
Zuzana Ilečková <yes>
2025-06-08 09:45:50 +0200

    úprava názvu.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,31 +1,28 @@
 import './Header.css';
-import ZUZEL from '../../pages/HomePage/img/Zuzel.png';
+import ZUZEL from '../../pages/HomePage/img/ZUZEL.png';
 import sound_on from './img/sound_on.png';
 import mute from './img/mute.png';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Header = () => {
-    const [sound, setSound] = useState(true)
+  const [sound, setSound] = useState(true);
 
-    const toggleSound = () => {
-        setSound(!sound)
-    }
+  const toggleSound = () => {
+    setSound(!sound);
+  };
 
-    return (
-        <div className="header">
-            <Link to='/'>
-                <img
-                    className="logo"
-                    src={ZUZEL}
-                    alt="logo ZUZEL" />
-            </Link>
-            <img
-                onClick={toggleSound}
-                className="sound"
-                src={sound ? sound_on : mute}
-                alt={sound ? "zvuk zapnuto" : "zvuk vypnuto"}
-            />
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <Link to="/">
+        <img className="logo" src={ZUZEL} alt="logo ZUZEL" />
+      </Link>
+      <img
+        onClick={toggleSound}
+        className="sound"
+        src={sound ? sound_on : mute}
+        alt={sound ? 'zvuk zapnuto' : 'zvuk vypnuto'}
+      />
+    </div>
+  );
+};
