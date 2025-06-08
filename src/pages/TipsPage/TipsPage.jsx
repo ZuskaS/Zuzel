9bff08898fd0a59a03925e4e3e489fad152ae7c0
Zuzana Ilečková <yes>
2025-06-08 09:40:44 +0200

    úprava názvu.

--- src/pages/TipsPage/TipsPage.jsx
+++ src/pages/TipsPage/TipsPage.jsx
@@ -2,71 +2,73 @@ import gear from './img/gear.png';
 import mute from '../../Components/Header/img/mute.png';
 import './TipsPage.css';
 import sound_on from '../../Components/Header/img/sound_on.png';
-import ZUZEL from '../HomePage/img/Zuzel.png';
+import ZUZEL from '../HomePage/img/ZUZEL.png';
 import { PageTitle } from '../../Components/PageTitle/PageTitle';
 import { Button } from '../../Components/Button/Button';
-import arrowRight from '../../img/arrow-right.png'
-import arrowLeft from '../../img/arrow-left.png'
+import arrowRight from '../../img/arrow-right.png';
+import arrowLeft from '../../img/arrow-left.png';
 
 import { useState } from 'react';
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export const TipsPage = () => {
   const [page, setPage] = useState(true);
 
   return page ? (
-        <div className="tips-1">
-          <PageTitle>
-            <img className="icon_gear" src={gear} alt="ikona ozubeného kola" />
-            <p>Tipy k ovládání aplikace</p>
-          </PageTitle>
+    <div className="tips-1">
+      <PageTitle>
+        <img className="icon_gear" src={gear} alt="ikona ozubeného kola" />
+        <p>Tipy k ovládání aplikace</p>
+      </PageTitle>
 
-          <div className="info__text">
-            <p>
-              {' '}
-              Zvuky aplikace lze zapnout nebo vypnout ikonou v pravém horním
-              rohu.{' '}
-            </p>
-            <div className="sound_icons">
-              <img className="icon_sound" src={sound_on} alt="ikona zvuku" />
-              <img className="mute" src={mute} alt="ikona vypnutého zvuku" />
-            </div>
-          </div>
-
-          <div className="info__text">
-            <p>Ikona loga v levém horním rohu Vás vrátí na úvodní stranu.</p>
-            <img className="img__logo" src={ZUZEL} alt="logo ZUZEL" />
-          </div>
-
-          <Button
-            onClick={() => setPage(false)}
-            text="další"
-            image={arrowRight}
-            position="right"
-          />
+      <div className="info__text">
+        <p>
+          {' '}
+          Zvuky aplikace lze zapnout nebo vypnout ikonou v pravém horním rohu.{' '}
+        </p>
+        <div className="sound_icons">
+          <img className="icon_sound" src={sound_on} alt="ikona zvuku" />
+          <img className="mute" src={mute} alt="ikona vypnutého zvuku" />
         </div>
-      ) : (
-        <div className="tips-2">
-          <div className="info__text">
-            <p>
-              Pokud aplikace nereaguje, ujistěte se, že se obrazovky nedotýkáte
-              jinými prsty, např. při držení telefonu.
-            </p>
-          </div>
+      </div>
 
-                <div className='tips-2'>
-                    <div className="info__text">
-                        <p>Pokud aplikace nereaguje, ujistěte se, že se obrazovky nedotýkáte jinými prsty, např. při držení telefonu.</p>
-                    </div>
+      <div className="info__text">
+        <p>Ikona loga v levém horním rohu Vás vrátí na úvodní stranu.</p>
+        <img className="img__logo" src={ZUZEL} alt="logo ZUZEL" />
+      </div>
 
-                    <div className="info__text">
-                        <p>Pro pohodlnější ovládání doporučujeme použít dotykové pero.</p>
-                    </div>
+      <Button
+        onClick={() => setPage(false)}
+        text="další"
+        image={arrowRight}
+        position="right"
+      />
+    </div>
+  ) : (
+    <div className="tips-2">
+      <div className="info__text">
+        <p>
+          Pokud aplikace nereaguje, ujistěte se, že se obrazovky nedotýkáte
+          jinými prsty, např. při držení telefonu.
+        </p>
+      </div>
 
-                    <Link to='/instruction'>
-                    <Button text='další' image={arrowLeft} position='left' />
-                    </Link>
+      <div className="tips-2">
+        <div className="info__text">
+          <p>
+            Pokud aplikace nereaguje, ujistěte se, že se obrazovky nedotýkáte
+            jinými prsty, např. při držení telefonu.
+          </p>
+        </div>
 
-                </div>
-                </div>
-            )}
+        <div className="info__text">
+          <p>Pro pohodlnější ovládání doporučujeme použít dotykové pero.</p>
+        </div>
+
+        <Link to="/instruction">
+          <Button text="další" image={arrowLeft} position="left" />
+        </Link>
+      </div>
+    </div>
+  );
+};
